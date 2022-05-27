f622d327b04dff85351f39a3274a110de57b1525
yoinercoding <yoinerbello1998@gmail.com>
2022-05-27 13:55:16 -0300

    Se agrega nav

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'nav', component: NavComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'nav'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
