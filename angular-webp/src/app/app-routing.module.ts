6a51b259263c08ec8f7371f87ad41dba9219aa8e
ybello <yoinercoding@gmail.com>
2022-09-05 14:01:05 -0300

    Se configuran rutas

--- angular-webp/src/app/app-routing.module.ts
+++ angular-webp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full'},
-  { path: 'main', component: MainComponent}
+  { path: 'main', component: MainComponent},
+  { path: 'footer', component: FooterComponent}
 ];
 
 @NgModule({
--- angular-webp/src/app/app.module.ts
+++ angular-webp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
