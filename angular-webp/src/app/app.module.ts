777f1814252b74af11227b449f16ecba5f9ee04d
ybello <yoinercoding@gmail.com>
2022-09-05 14:46:41 -0300

    Se elimina rutas de componentes

--- angular-webp/src/app/app.module.ts
+++ angular-webp/src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
-import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    FooterComponent,
-    LoaderComponent
   ],
   imports: [
     BrowserModule,
