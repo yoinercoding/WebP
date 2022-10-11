1da886d1355638789ca4065e27dd57304bc7d997
ybello <yoinercoding@gmail.com>
2022-10-11 16:01:57 -0300

    Se agregan 3 componentes

--- angular-webp/src/app/app.module.ts
+++ angular-webp/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { InfoComponent } from './info/info.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
+    InfoComponent,
+    TecnologiasComponent,
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
