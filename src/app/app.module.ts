309560aad44f45d895aa741cc07c3b3b4762b3a3
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-01-16 10:27:10 -0300

    updatee

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { InfoComponent } from './info/info.component';
+import { MainComponent } from './main/main.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactoComponent,
+    InfoComponent,
+    MainComponent,
+    TecnologiasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
