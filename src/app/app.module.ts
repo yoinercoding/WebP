d3703915e1a8c56d6436dbc33fdc9dd6c02d0386
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-01-23 10:58:01 -0300

    Se agregan imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { ContactoComponent } from './contacto/contacto.component';
 import { InfoComponent } from './info/info.component';
 import { MainComponent } from './main/main.component';
 import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { TecnologiasComponent } from './tecnologias/tecnologias.component';
     ContactoComponent,
     InfoComponent,
     MainComponent,
-    TecnologiasComponent
+    TecnologiasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
