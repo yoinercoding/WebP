ec089578dc2d1b3aec1ebe496b02b5e4d45b5312
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-01-23 10:55:40 -0300

    Routing de Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ContactoComponent } from './contacto/contacto.component';
 import { InfoComponent } from './info/info.component';
 import { MainComponent } from './main/main.component';
 import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full'},
   { path: 'main', component: MainComponent},
   { path: 'info', component: InfoComponent},
   { path: 'tecnologias', component: TecnologiasComponent},
-  { path: 'contacto', component: ContactoComponent}
+  { path: 'contacto', component: ContactoComponent},
+  { path: 'footer', component: FooterComponent}
 ];
 
 @NgModule({
