28e67a130b1c722052690d51957c2d6be2e2bba1
Yoiner Bello Fajardo <ybello@imasa.com.ar>
2023-03-06 16:44:39 -0300

    proyecto casi terminado, falta spinner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactoComponent } from './contacto/contacto.component';
 import { InfoComponent } from './info/info.component';
-import { MainComponent } from './main/main.component';
-import { OnToTopComponent } from './on-to-top/on-to-top.component';
 import { FooterComponent } from './footer/footer.component';
-import { ProyectosComponent } from './proyectos/proyectos.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full'},
-  { path: 'main', component: MainComponent},
+  { path: '', redirectTo: 'info', pathMatch: 'full'},
+  { path: '**', redirectTo: 'info', pathMatch: 'full'},
   { path: 'info', component: InfoComponent},
-  { path: 'contacto', component: ContactoComponent},
   { path: 'footer', component: FooterComponent},
-  { path: 'onToTop', component: OnToTopComponent},
-  { path: 'proyectos', component: ProyectosComponent}
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContactoComponent } from './contacto/contacto.component';
 import { InfoComponent } from './info/info.component';
-import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
-import { OnToTopComponent } from './on-to-top/on-to-top.component';
-import { ProyectosComponent } from './proyectos/proyectos.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactoComponent,
     InfoComponent,
-    MainComponent,
     FooterComponent,
-    OnToTopComponent,
-    ProyectosComponent
   ],
   imports: [
     BrowserModule,
